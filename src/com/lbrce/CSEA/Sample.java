c8a35365e007be4ece7f693d887a24e68252d764
Admin <Admin@DESKTOP-P7UG078>
2023-09-21 14:02:13 +0530

    TeamLead commit

---
+++ src/com/lbrce/CSEA/Sample.java
@@ -0,0 +1,14 @@
+package com.lbrce.CSEA;
+
+public class Sample {
+
+	public static void janu() {
+		// TODO Auto-generated method stub
+         System.out.println("This is TeamLead:Janu");
+	}
+	public static void main(String args[])
+	{
+		janu();
+	}
+
+}
